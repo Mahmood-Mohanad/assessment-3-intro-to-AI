fcba5b7b3a1a22d770e81bdd0f99cfeb19acbe63
Mahmood Mohanad <mahmoodmohanadalsuhail@gmail.com>
2025-08-20 01:49:55 +0300

    Adding a simple behavior tree with three tasks (find random point, move to it, wait) and adding a navmesh

---
+++ Source/A3_AI/BTTask_FindRandomLocation.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_FindRandomLocation.h"
+#include "NavigationSystem.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "NPC_AIController.h"
+
+UBTTask_FindRandomLocation::UBTTask_FindRandomLocation(FObjectInitializer const& ObjectInitializer)
+{
+	NodeName = "Find Random Location In NavMesh";
+}
+
+EBTNodeResult::Type UBTTask_FindRandomLocation::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	//AI controller and its NPC
+	if (auto* const cont = Cast<ANPC_AIController>(OwnerComp.GetAIOwner()))
+	{
+		if (auto* const npc = cont->GetPawn())
+		{
+			//having the original NPC location
+			auto const Origin = npc->GetActorLocation();
+
+			//getting the navigation system and genrate random location in it
+			if (auto* const NavSys = UNavigationSystemV1::GetCurrent(GetWorld()))
+			{
+				FNavLocation Loc;
+				if (NavSys->GetRandomPointInNavigableRadius(Origin, SearchRadius, Loc))
+				{
+					OwnerComp.GetBlackboardComponent()->SetValueAsVector(GetSelectedBlackboardKey(), Loc.Location);
+				}
+
+				// finish with success
+				FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+				return EBTNodeResult::Succeeded;
+			}
+		}
+	}
+	return EBTNodeResult::Failed;
+}
